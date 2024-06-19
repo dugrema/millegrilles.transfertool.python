9021168be05ba338af259c714d80616126baf058
mathieu <mathieu.dugre@mdugre.info>
2024-06-19 11:54:59 -0400

    Navigation repertoires

--- tksample1/Navigation.py
+++ tksample1/Navigation.py
@@ -1,7 +1,11 @@
 import logging
 import tkinter as tk
+from tkinter import ttk
 import json
 import multibase
+import datetime
+import pathlib
+import pytz
 
 from typing import Optional
 from threading import Event, Thread
@@ -12,6 +16,13 @@ from tksample1.AuthUsager import Authentification
 
 
 from millegrilles_messages.chiffrage.DechiffrageUtils import dechiffrer_reponse, dechiffrer_document_secrete
+
+
+class Repertoire:
+
+    def __init__(self, fichiers: list, cuuid: Optional[str] = None):
+        self.fichiers = fichiers
+        self.cuuid = cuuid
 
 
 class Navigation:
@@ -25,19 +36,61 @@ class Navigation:
 
         self.__event_dirty = Event()
 
-        self.__tuuid_courant = None
+        self.breadcrumb = list()
+
+        self.__cuuid_a_charger = None
+        self.__repertoire = None
 
         self.__thread = Thread(name="Navigation", target=self.run)
         self.__thread.start()
 
-    def charger_tuuid(self, tuuid: Optional[str] = None):
-        self.__event_dirty.clear()
+    def changer_cuuid(self, cuuid: Optional[str] = None):
+        self.__cuuid_a_charger = cuuid
+        self.__event_dirty.set()
 
-        if tuuid is None:
-            reponse = sync_collection(self.connexion)
-            self.__tuuid_courant = True  # Favoris
+    def __charger_cuuid(self, cuuid: Optional[str] = None):
+        # self.__event_dirty.clear()
+        cuuid = cuuid or self.__cuuid_a_charger
+        self.__cuuid_a_charger = None
+
+        if cuuid is None:
+            self.__repertoire = sync_collection(self.connexion)
         else:
-            raise NotImplementedError()
+            try:
+                if self.breadcrumb[-1]['tuuid'] != cuuid:
+                    append_cuuid = True
+                else:
+                    append_cuuid = False
+            except IndexError:
+                append_cuuid = True
+
+            if append_cuuid:
+                # Changer breadcrumb, ajouter repertoire selectionne
+                repertoire = [c for c in self.__repertoire.fichiers if c['tuuid'] == cuuid].pop()
+                self.breadcrumb.append(repertoire)
+                breadcrumb_path = [p['nom'] for p in self.breadcrumb]
+                breadcrumb_path = pathlib.Path('favoris', *breadcrumb_path)
+                self.nav_frame.set_breadcrumb(breadcrumb_path)
+
+            # Recuperer contenu du repertoire
+            self.__repertoire = sync_collection(self.connexion, cuuid)
+
+        self.nav_frame.afficher_repertoire(self.__repertoire)
+
+    def naviguer_up(self):
+        if len(self.breadcrumb) == 0:
+            return
+        self.breadcrumb = self.breadcrumb[:-1]
+        breadcrumb_path = [p['nom'] for p in self.breadcrumb]
+        self.nav_frame.set_breadcrumb(pathlib.Path('favoris', *breadcrumb_path))
+
+        # Naviguer vers
+        try:
+            self.__cuuid_a_charger = self.breadcrumb[-1]['tuuid']
+        except (KeyError, IndexError, AttributeError):
+            self.__cuuid_a_charger = None
+
+        self.__event_dirty.set()
 
     def run(self):
         self.__event_dirty.set()
@@ -48,11 +101,10 @@ class Navigation:
                 return  # Stopping
 
             self.__event_dirty.wait()
+            self.__event_dirty.clear()
 
-            # Determiner action
-            if self.__tuuid_courant is None:
-                # Charger les favoris
-                self.charger_tuuid()
+            # Charger le repertoire
+            self.__charger_cuuid()
 
 
 class NavigationFrame(tk.Frame):
@@ -61,9 +113,74 @@ class NavigationFrame(tk.Frame):
         super().__init__(*args, **kwargs)
         self.__logger = logging.getLogger(__name__ + '.' + self.__class__.__name__)
         self.__navigation = navigation
+        self.__repertoire: Optional[Repertoire] = None
+
+        self.__breadcrumb_path = pathlib.Path('Favoris/')
+        self.breadcrumb = tk.StringVar(master=self, value=str(self.__breadcrumb_path))
+        self.__breadcrumb_label = tk.Label(master=self, textvariable=self.breadcrumb, justify="left")
+        self.__btn_up = tk.Button(master=self, text="Up", command=self.btn_up_handler)
+
+        self.dirlist = ttk.Treeview(master=self, columns=('taille', 'type', 'date'))
+        self.dirlist['columns'] = ('taille', 'type', 'date')
+
+        self.widget_bind()
+
+    def widget_bind(self):
+        self.dirlist.bind('<Button-3>', self.dirlist_rightclick_fichier)
+        self.dirlist.bind('<Double-Button-1>', self.dirlist_doubleclick_fichier)
 
     def pack(self):
+        self.__breadcrumb_label.pack()
+        self.__btn_up.pack()
+        self.dirlist.pack()
         super().pack()
+
+    def btn_up_handler(self):
+        self.__navigation.naviguer_up()
+
+    def set_breadcrumb(self, breadcrumb: pathlib.Path):
+        self.breadcrumb.set(str(breadcrumb))
+
+    def afficher_repertoire(self, repertoire: Repertoire):
+        self.__repertoire = repertoire
+        children = self.dirlist.get_children()
+        if len(children) > 0:
+            for c in children:
+                self.dirlist.delete(c)
+
+        def sort_nom(item):
+            if item['type_node'] == 'Fichier':
+                tn = '2'
+            else:
+                tn = '1'
+            return tn + item['nom']
+
+        fichiers_tries = sorted(self.__repertoire.fichiers, key=sort_nom)
+
+        for fichier in fichiers_tries:
+            nom_fichier = fichier['nom']
+            taille_fichier = ''
+            type_node = fichier['type_node']
+            tuuid = fichier['tuuid']
+            if type_node in ['Collection', 'Repertoire']:
+                type_fichier = 'Repertoire'
+                date_fichier = datetime.datetime.fromtimestamp(fichier['derniere_modification'], tz=pytz.UTC)
+            else:
+                type_fichier = 'Fichier'
+                date_fichier = None
+            self.dirlist.insert('', 'end', iid=tuuid, text=nom_fichier, values=(taille_fichier, type_fichier, date_fichier))
+
+    def dirlist_rightclick_fichier(self, event):
+        pass
+
+    def dirlist_doubleclick_fichier(self, event):
+        tuuid = self.dirlist.focus()
+        item = self.dirlist.item(tuuid)
+        values = item['values']
+        if values[1] != 'Fichier':
+            self.__navigation.changer_cuuid(tuuid)
+        else:
+            raise NotImplementedError('todo - download fichier')
 
 
 def sync_collection(connexion, cuuid: Optional[str] = None):
@@ -86,8 +203,12 @@ def sync_collection(connexion, cuuid: Optional[str] = None):
     if contenu_sync['complete'] is False:
         raise NotImplementedError('TODO : liste incomplete')
 
-    favoris = [f for f in contenu_sync['liste'] if f['supprime'] is False]
-    tuuids = [f['tuuid'] for f in favoris]
+    fichiers_recus = [f for f in contenu_sync['liste'] if f['supprime'] is False]
+    tuuids = [f['tuuid'] for f in fichiers_recus]
+
+    if len(tuuids) == 0:
+        # Aucun fichier a charger (repertoire vide)
+        return Repertoire(list(), cuuid)
 
     # Charger les documents
     requete_documents = {'tuuids_documents': tuuids, 'partage': False, 'contact_id': None}
@@ -107,14 +228,6 @@ def sync_collection(connexion, cuuid: Optional[str] = None):
     cle_ids = list(cle_ids)
 
     # Charger les cles de dechiffrage
-    #   const params = { fuuids, partage, version: 2 }
-    #   return connexionClient.emitWithAck(
-    #     'getPermissionCles', params,
-    #     {
-    #       kind: MESSAGE_KINDS.KIND_REQUETE, domaine: CONST_DOMAINE_GROSFICHIERS, action: 'getClesFichiers',
-    #       timeout: 30_000, ajouterCertificat: true
-    #     }
-    #   )
     requete_cles = {'fuuids': cle_ids, 'partage': False, 'version': 2}
     requete_cles, message_id = connexion.formatteur.signer_message(
         Constantes.KIND_REQUETE, requete_cles, 'GrosFichiers', True, 'getClesFichiers')
@@ -136,4 +249,6 @@ def sync_collection(connexion, cuuid: Optional[str] = None):
 
         fichier.update(info_dechiffree)
 
-    pass
+    rep = Repertoire(fichiers, cuuid)
+
+    return rep
